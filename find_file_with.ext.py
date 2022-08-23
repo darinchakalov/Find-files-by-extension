f6f22cdc1d844e6c145287003e001ff3d5364161
darinchakalov <darinchakalov@hotmail.com>
2022-08-23 11:56:06 +0300

    refactoring

--- find_file_with.ext.py
+++ find_file_with.ext.py
@@ -4,18 +4,18 @@ from pathlib import Path
 
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
 
 
-DOWNLOADS_FOLDER = str(Path.home() / 'Downloads')
+DOWNLOADS_FOLDER = str(Path.home() / "Downloads")
 os.chdir(DOWNLOADS_FOLDER)
 CWD = os.getcwd()
 
@@ -24,25 +24,20 @@ CWD = os.getcwd()
 # TODO Move files -> list available dirs and choose one or create new one to move files to
 # TODO auto organizer -> create dirs named with files extension and move each file with that extension into it
 # DOne Add menu to list all available extensions
+# TODO Add comments
 
 main_menu_options = {
-    1: 'Delete files',
-    2: 'Search in files',
-    3: 'Move files',
-    4: 'Search for another extension',
-    5: 'List all extensions in the folder -- TODO',
-    6: 'Exit',
+    1: "Delete files",
+    2: "Search in files",
+    3: "Move files",
+    4: "Search for another extension",
+    5: "List all extensions in the folder -- TODO",
+    6: "Exit",
 }
 
-delete_files_options = {
-    1: 'Yes',
-    2: 'No - go back'
-}
+delete_files_options = {1: "Yes", 2: "No - go back"}
 
-search_files_options = {
-    1: 'Yes',
-    2: 'No - go back'
-}
+search_files_options = {1: "Yes", 2: "No - go back"}
 
 
 global file_list
@@ -63,7 +58,8 @@ def fill_file_list(ext):
 
 def extension_select_menu():
     ext = input(
-        f"{bcolors.HEADER}Please specify file extension: {bcolors.ENDC}").lower()
+        f"{bcolors.HEADER}Please specify file extension: {bcolors.ENDC}"
+    ).lower()
 
     fill_file_list(ext)
 
@@ -71,7 +67,8 @@ def extension_select_menu():
         if len(file_list) == 0:
             print("No files with that extension found. Try another")
             ext = input(
-                f"{bcolors.HEADER}Please specify another file extension: {bcolors.ENDC}").lower()
+                f"{bcolors.HEADER}Please specify another file extension: {bcolors.ENDC}"
+            ).lower()
 
             fill_file_list(ext)
         else:
@@ -80,7 +77,7 @@ def extension_select_menu():
 
 
 def print_menu(options):
-    print(f'{bcolors.OKBLUE}Select option: {bcolors.ENDC}')
+    print(f"{bcolors.OKBLUE}Select option: {bcolors.ENDC}")
     for key in options.keys():
         print(f"{bcolors.UNDERLINE}{key} -- {options[key]}{bcolors.ENDC}")
 
@@ -89,7 +86,7 @@ def delete_files():
     for file in file_list:
         try:
             os.remove(file)
-            print(f'{bcolors.OKGREEN}{file} deleted. {bcolors.ENDC}')
+            print(f"{bcolors.OKGREEN}{file} deleted. {bcolors.ENDC}")
         except:
             print(f"The file {file} doesn't exist or could not be deleted")
             pass
@@ -97,26 +94,26 @@ def delete_files():
 
 def search_in_files(search_string):
     for fname in file_list:
-        f = open(fname, 'r', encoding='utf8')
+        f = open(fname, "r", encoding="utf8")
 
         if search_string in f.read():
             print(
                 f"Found search string in {bcolors.OKGREEN}{fname}{bcolors.ENDC}")
             print(f"{bcolors.BOLD}Results in the file: {bcolors.ENDC}")
 
-            with open(fname, encoding='utf8') as f:
+            with open(fname, encoding="utf8") as f:
                 lines = [line.rstrip() for line in f]
 
             count = 0
             for line in lines:
                 if search_string in line:
-                    count += 1
+                    count = 1
                     print(f"Line {count}: {line}")
             print()
 
 
 def move_files():
-    print(f'{bcolors.WARNING}Files moved. {bcolors.ENDC}')
+    print(f"{bcolors.WARNING}Files moved. {bcolors.ENDC}")
 
 
 def delete_files_menu():
@@ -124,9 +121,9 @@ def delete_files_menu():
         print_menu(delete_files_options)
         try:
             option = int(
-                input(f'{bcolors.OKBLUE}Please select option: {bcolors.ENDC}'))
+                input(f"{bcolors.OKBLUE}Please select option: {bcolors.ENDC}"))
         except:
-            print('Wrong input. Please enter a number ...')
+            print("Wrong input. Please enter a number ...")
 
         if option == 1:
             delete_files()
@@ -137,19 +134,22 @@ def delete_files_menu():
 
 def search_in_files_menu():
     search_string = input(
-        f"{bcolors.OKCYAN}Please specify string you want to look for into the selected files: {bcolors.ENDC}")
+        f"{bcolors.OKCYAN}Please specify string you want to look for into the selected files: {bcolors.ENDC}"
+    )
     while True:
 
         search_in_files(search_string)
         print_menu(search_files_options)
         try:
             option = int(
-                input(f'{bcolors.OKBLUE}Search another string? {bcolors.ENDC}'))
+                input(f"{bcolors.OKBLUE}Search another string? {bcolors.ENDC}")
+            )
         except:
-            print('Wrong input. Please enter a number ...')
+            print("Wrong input. Please enter a number ...")
         if option == 1:
             search_string = input(
-                f"{bcolors.OKCYAN}Please specify string you want to look for into the selected files: {bcolors.ENDC}")
+                f"{bcolors.OKCYAN}Please specify string you want to look for into the selected files: {bcolors.ENDC}"
+            )
             search_in_files(search_string)
         else:
             break
@@ -160,12 +160,12 @@ def main():
     while True:
         print()
         print_menu(main_menu_options)
-        option = ''
+        option = ""
         try:
             option = int(
-                input(f'{bcolors.OKBLUE}Please select option: {bcolors.ENDC}'))
+                input(f"{bcolors.OKBLUE}Please select option: {bcolors.ENDC}"))
         except:
-            print('Wrong input. Please enter a number ...')
+            print("Wrong input. Please enter a number ...")
 
         if option == 1:
             delete_files_menu()
@@ -181,23 +181,22 @@ def main():
             extensions = {}
             for file in os.listdir(CWD):
                 if isfile(file):
-                    file_name_splitter = file.split('.')
+                    file_name_splitter = file.split(".")
                     if len(file_name_splitter) > 1:
                         extension = file_name_splitter[-1]
-                        if extension in extensions.keys():
+                        if extension in extensions:
                             extensions[extension] += 1
                         else:
                             extensions[extension] = 1
 
-            
             for ext, count in extensions.items():
                 print(f"{ext}: found {count}")
 
         elif option == 6:
-            print('Exiting script...')
+            print("Exiting script...")
             exit()
         else:
-            print('Wrong input. Please enter number from 1 to 4 to select action')
+            print("Wrong input. Please enter number from 1 to 4 to select action")
 
 
 if __name__ == "__main__":
